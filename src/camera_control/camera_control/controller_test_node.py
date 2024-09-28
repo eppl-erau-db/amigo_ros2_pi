3c560728fa133bd04efdd4c0db61d0bcdf515b11
Gabriel Rodriguez <gabearod2@gmail.com>
2024-09-28 19:34:03 -0400

    Adding stepper motor test node with new step amount

--- src/camera_control/camera_control/controller_test_node.py
+++ src/camera_control/camera_control/controller_test_node.py
@@ -17,10 +17,9 @@ class StepperMotorNode(Node):
         self.motor2.SetMicroStep('software', 'fullstep')
         
         # Timer to handle motor control
-        self.timer = self.create_timer(2.0, self.move_motor_up(self.motor1))
     
     def move_motor_up(self, motor):
-        motor.TurnStep(Dir='forward', steps=2048, stepdelay=0.005)
+        motor.TurnStep(Dir='forward', steps=20, stepdelay=0.005)
         print("Motor moved positive")
     
     def move_motor_down(self, motor):
