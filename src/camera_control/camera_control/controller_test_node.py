49a6d278d5c280b060bd773ed27c26a483df384c
Gabriel Rodriguez <138304753+gabearod2@users.noreply.github.com>
2025-02-14 11:48:20 -0500

    Microstep change

--- src/camera_control/camera_control/controller_test_node.py
+++ src/camera_control/camera_control/controller_test_node.py
@@ -23,8 +23,8 @@ class StepperMotorNode(Node):
 
     def move_motor_forward(self):
         for i in range(100):
-            self.kit.stepper1.onestep(style=stepper.SINGLE, direction=stepper.FORWARD)
-            time.sleep(0.01)
+            self.kit.stepper1.onestep(style=stepper.MICROSTEP, direction=stepper.FORWARD)
+            #time.sleep(0.01)
         print("Stepper Motor 1 Moved FORWARD")
     
     def move_motor_backward(self):
