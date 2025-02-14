5e514a3f650f68187d6641daa0752521e1487ca0
Gabriel Rodriguez <138304753+gabearod2@users.noreply.github.com>
2025-02-14 17:22:02 -0500

    Adding full pitch and heading control

--- src/camera_control/camera_control/scan_controller_node.py
+++ src/camera_control/camera_control/scan_controller_node.py
@@ -12,41 +12,50 @@ class CameraScanNode(Node):
         
         # Initialize the Motor Kit
         self.kit = MotorKit(i2c=board.I2C())
-
-        self.kit.stepper1 = stepper.StepperMotor()
         
-        # Setting a timer to perform a scan every 20 seconds 
-        self.timer_period = 20.0 
+        # Setting a timer to perform a scan every 10 seconds 
+        self.timer_period = 10.0 
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
 
         # Initializing tracking variables
         self.absolute_heading = 0
         self.absolute_pitch = 0
+        self.initial_heading = 0
+        self.initial_pitch = 0
         self.heading_microsteps = 0
         self.pitch_microsteps = 0 
         self.angle_microstep = 1.8/16 # degrees
             
     def timer_callback(self):
-        for i in range(100):
+        for i in range(50): # 45 degrees
             self.heading_move_ccw()
+
+        for i in range(50): # 45 degrees
+            self.heading_move_cw()
+        
+        for i in range(50): # 45 degrees
+            self.pitch_move_ccw()
+        
+        for i in range(50): # 45 degrees
+            self.pitch_move_cw()
         
     def heading_move_ccw(self):
-        self.heading_microsteps = self.kit.stepper1.onestep(style=stepper.MICROSTEP, direction=stepper.FORWARD)
-        self.absolute_heading = self.absolute_heading + self.heading_microsteps * self.angle_microstep
+        self.heading_microsteps = self.kit.stepper1.onestep(style=stepper.INTERLEAVE, direction=stepper.FORWARD)
+        self.absolute_heading = self.heading_microsteps * self.angle_microstep
         self.get_logger().info(f'The Current Absolute Heading:{self.absolute_heading}')
 
     def heading_move_cw(self):
-        self.heading_microsteps = self.kit.stepper1.onestep(style=stepper.MICROSTEP, direction=stepper.FORWARD)
-        self.absolute_heading = self.absolute_heading - self.heading_microsteps * self.angle_microstep
+        self.heading_microsteps = self.kit.stepper1.onestep(style=stepper.INTERLEAVE, direction=stepper.BACKWARD)
+        self.absolute_heading = - self.heading_microsteps * self.angle_microstep
         self.get_logger().info(f'The Current Absolute Heading:{self.absolute_heading}')
 
     def pitch_move_ccw(self):
-        self.pitch_microsteps = self.kit.stepper2.onestep(style=stepper.MICROSTEP, direction=stepper.FORWARD)
-        self.absolute_pitch = self.absolute_pitch + self.pitch_microsteps * self.angle_microstep
+        self.pitch_microsteps = self.kit.stepper2.onestep(style=stepper.INTERLEAVE, direction=stepper.BACKWARD)
+        self.absolute_pitch = self.pitch_microsteps * self.angle_microstep
         self.get_logger().info(f'The Current Absolute Pitch:{self.absolute_pitch}')
     
     def pitch_move_cw(self):
-        self.heading_microsteps = self.kit.stepper2.onestep(style=stepper.MICROSTEP, direction=stepper.FORWARD)
+        self.heading_microsteps = self.kit.stepper2.onestep(style=stepper.INTERLEAVE, direction=stepper.BACKWARD)
         self.absolute_pitch = self.absolute_heading + self.pitch_microsteps * self.angle_microstep
         self.get_logger().info(f'The Current Absolute Pitch:{self.absolute_pitch}')
     
