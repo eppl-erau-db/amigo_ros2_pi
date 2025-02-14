cdb0f3d90419976a666e46bc00ee21c914bc5a9a
Gabriel Rodriguez <138304753+gabearod2@users.noreply.github.com>
2025-02-14 18:08:49 -0500

    Creating searching pattern

--- src/camera_control/camera_control/scan_controller_node.py
+++ src/camera_control/camera_control/scan_controller_node.py
@@ -14,37 +14,54 @@ class CameraScanNode(Node):
         self.kit = MotorKit(i2c=board.I2C())
         
         # Setting a timer to perform a scan every 10 seconds 
-        self.timer_period = 10.0 
-        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        self.timer = self.create_timer(10.0, self.timer_callback)
 
         # Initializing tracking variables
-        self.absolute_heading = 0
-        self.absolute_pitch = 0
-        self.initial_heading = 0
-        self.initial_pitch = 0
+        self.heading = 0
+        self.pitch = 0
         self.heading_microsteps = 0
         self.pitch_microsteps = 0 
         self.angle_microstep = 1.8/16 # degrees
+
+        # Scan positions (heading, pitch)
+        self.scan_positions = [
+            (-135, 0), (-135, 45), (-90, 45), (-90, 0),
+            (-45, 0), (-45, 45), (0, 45), (0, 0),
+            (45, 0), (45, 45), (90, 45), (90, 0),
+            (135, 0), (135, 45), (0, 0)
+        ]
+        self.scan_index = 0
+
+        # Timer for moving between scan positions every 0.5 seconds
+        self.create_timer(0.5, self.next_scan_step)
             
     def timer_callback(self):
-        for i in range(50): # 45 degrees
-            self.heading_move_ccw()
+        """ Reset scan cycle when the timer triggers. """
+        self.scan_index = 0  # Reset to start position
         
-        time.sleep(0.5)
+    def next_scan_step(self):
+        """ Command the camera to the next position """
+        if self.scan_index < len(self.scan_positions):
+            target_heading, target_pitch = self.scan_positions[self.scan_index]
+            self.move_camera(target_heading, target_pitch)
+            self.scan_index += 1
 
-        for i in range(50): # 45 degrees
-            self.heading_move_cw()
-        
-        time.sleep(0.5)
-        
-        for i in range(50): # 45 degrees
-            self.pitch_move_ccw()
+    def move_camera(self, target_heading, target_pitch):
 
-        time.sleep(0.5)
-        
-        for i in range(50): # 45 degrees
-            self.pitch_move_cw()
-        
+        if self.heading < target_heading:
+            while self.heading != target_heading:
+                self.heading_move_ccw()
+        elif self.heading > target_heading:
+            while self.heading != target_heading:
+                self.heading_move_cw()
+
+        if self.pitch < target_pitch:
+            while self.pitch != target_pitch:
+                self.pitch_move_ccw()
+        elif self.pitch > target_pitch:
+            while self.pitch != target_pitch:
+                self.pitch_move_cw()
+
     def heading_move_ccw(self):
         self.heading_microsteps = self.kit.stepper1.onestep(style=stepper.INTERLEAVE, direction=stepper.FORWARD)
         self.absolute_heading = self.heading_microsteps * self.angle_microstep
